45fb1f06e634e20df50c5dacc77a78e78b7fa03e
Oliver Tacke <o.tacke@posteo.de>
2024-04-11 17:47:34 +0200

    Modernize build chain

--- src/entries/dist.js
+++ src/entries/dist.js
@@ -1,5 +1,5 @@
 import '@styles/h5peditor-3d-model-loader.scss';
-import ThreeDModelLoader from '@scripts/h5peditor-3d-model-loader';
+import ThreeDModelLoader from '@scripts/h5peditor-3d-model-loader.js';
 
 // Load library
 H5PEditor.widgets.threeDModelLoader = ThreeDModelLoader;
